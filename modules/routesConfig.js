43006b16745a98869f80a7b186ef2ed40311ad20
Anthony Lin <anthony.lin.198@gmail.com>
2020-12-02 13:11:47 -0800

    import apps

--- modules/routesConfig.js
+++ modules/routesConfig.js
@@ -1,41 +1,134 @@
 const routesConfig = [
   {
-    id: 'app',
-    title: 'Application',
-    messageId: 'sidebar.application',
-    type: 'group',
+    id: 'dashboards',
+    title: 'Your Profile',
+    messageId: 'My Profile',
+    type: 'collapse',
+    icon: 'dashboard',
     children: [
       {
-        id: 'dashboards',
-        title: 'Your Profile',
-        messageId: 'sidebar.app.dashboard',
-        type: 'collapse',
-        icon: 'dashboard',
-        children: [
-          {
-            id: 'healthCare',
-            title: 'Health Care',
-            messageId: 'sidebar.healthCare',
-            type: 'item',
-            url: '/dashboards/health-care',
-          },
-          {
-            id: 'e-commerce',
-            title: 'E-Commerce',
-            messageId: 'sidebar.app.dashboard.eCommerce',
-            type: 'item',
-            url: '/dashboards/e-commerce',
-          },
-        ],
+        id: 'healthCare',
+        title: 'Health Care',
+        messageId: 'View Profile',
+        type: 'item',
+        url: '/dashboards/health-care',
       },
       {
-        id: 'academy',
-        title: 'Academy',
-        messageId: 'sidebar.app.dashboard.academy',
+        id: 'e-commerce',
+        title: 'E-Commerce',
+        messageId: 'Edit Profile',
         type: 'item',
-        url: '/dashboards/academy',
+        url: '/dashboards/e-commerce',
       },
     ],
   },
+
+  {
+    id: 'healthCare',
+    title: 'Health Care',
+    messageId: 'Dashboard',
+    type: 'item',
+    url: '/dashboards/academy',
+  },
+  {
+    id: 'dashboards',
+    title: 'Your Profile',
+    messageId: 'Academy',
+    type: 'collapse',
+    icon: 'dashboard',
+    children: [
+      {
+        id: 'healthCare',
+        title: 'Health Care',
+        messageId: 'View Profile',
+        type: 'item',
+        url: '/dashboards/product_detail',
+      },
+      {
+        id: 'e-commerce',
+        title: 'E-Commerce',
+        messageId: 'Edit Profile',
+        type: 'item',
+        url: '/dashboards/customers',
+      },
+    ],
+  },
+  {
+    id: 'dashboards',
+    title: 'Your Profile',
+    messageId: 'Directory',
+    type: 'collapse',
+    icon: 'dashboard',
+    children: [
+      {
+        id: 'healthCare',
+        title: 'Health Care',
+        messageId: 'View Profile',
+        type: 'item',
+        url: '/dashboards/analytics',
+      },
+      {
+        id: 'e-commerce',
+        title: 'E-Commerce',
+        messageId: 'Messages',
+        type: 'item',
+        url: '/apps/chat',
+      },
+    ],
+  },
+
+  // {
+  //   id: 'app',
+  //   title: 'Academy',
+  //   messageId: 'Academy',
+  //   type: 'group',
+  //   children: [
+  //     {
+  //       id: 'Curriculum',
+  //       title: 'Academy',
+  //       messageId: 'Curriculum',
+  //       type: 'item',
+  //       url: '/dashboards/academy',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 'app',
+  //   title: 'Directory',
+  //   messageId: 'sidebar.application',
+  //   type: 'group',
+  //   children: [
+  //     {
+  //       id: 'dashboards',
+  //       title: 'Your Profile',
+  //       messageId: 'Directory',
+  //       type: 'collapse',
+  //       icon: 'dashboard',
+  //       children: [
+  //         {
+  //           id: 'healthCare',
+  //           title: 'Health Care',
+  //           messageId: 'sidebar.healthCare',
+  //           type: 'item',
+  //           url: '/dashboards/health-care',
+  //         },
+  //         {
+  //           id: 'e-commerce',
+  //           title: 'E-Commerce',
+  //           messageId: 'sidebar.app.dashboard.eCommerce',
+  //           type: 'item',
+  //           url: '/dashboards/e-commerce',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       id: 'academy',
+  //       title: 'Academy',
+  //       messageId: 'sidebar.app.dashboard.academy',
+  //       type: 'item',
+  //       url: '/dashboards/academy',
+  //     },
+  //   ],
+  // },
 ];
 export default routesConfig;
